01d25b7fbc950128048c51ad93b90fcd6ae8672f
Jaber <jaberc69@gmail.com>
2026-09-10 15:19:08 -0300

    Arquivo 06

--- 06/index.js
+++ 06/index.js
@@ -1,1 +1,8 @@
 const celular = 7199918888;
+const celularString = String(celular);
+
+const celularCorreto = `(${celularString.slice(0,2)}) 9${celularString.slice(2,6)}-${celularString.slice(6)}`;
+
+console.log(celularCorreto);
+
+
